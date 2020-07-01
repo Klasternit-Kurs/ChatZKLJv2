68c49e625a7ba53e1df4568099a731da1ce13817
Dragan Pepic <d.pepic@hotmail.com>
2020-07-01 20:53:44 +0200

    Add project files.

---
+++ BazeZklj/Server/Baza.cs
@@ -0,0 +1,24 @@
+﻿using BazeZklj.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BazeZklj.Server
+{
+	public class Baza : DbContext
+	{
+		public DbSet<Osoba> Osobas { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Osoba>().HasKey(o => o.ID);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-75VO5EN\TESTSERVER;Initial Catalog=AspBaza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+		}
+	}
+}
---
+++ BazeZklj/Server/Hab.cs
@@ -0,0 +1,43 @@
+﻿using BazeZklj.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BazeZklj.Server
+{
+	public class Hab : Hub
+	{
+		public void KaServeru(Osoba o)
+		{
+			Baza db = new Baza();
+			Console.WriteLine($"{o.ID} - {o.Ime} - {o.Prezime}");
+			var oso = db.Osobas.Find(o.ID);
+			if (oso == null)
+			{
+				db.Osobas.Add(o);
+			} else
+			{
+				db.Osobas.Remove(oso);
+				db.Osobas.Add(o);
+			}
+			db.SaveChanges();
+			PosaljiPodatke();
+		}
+
+		public void Obrisi(Osoba o)
+		{
+			Baza db = new Baza();
+			db.Osobas.Remove(o);
+			db.SaveChanges();
+			PosaljiPodatke();
+		}
+
+		public void PosaljiPodatke()
+		{
+			Baza db = new Baza();
+			Clients.All.SendAsync("KaKlijentu", db.Osobas.ToList());
+		}
+	}
+}
